62ea9d144f3f23a35d38f1ccb92d4ecfc8062138
lmmaa <lmmaa@iscte-iul.pt>
2018-10-26 20:28:10 +0100

    table_line teste

--- src/jUnitTests/frontend/Table_lineTest.java
+++ src/jUnitTests/frontend/Table_lineTest.java
@@ -24,8 +24,8 @@ public class Table_lineTest {
 
 	@Test
 	public void testGetMessage() throws Exception {
-		table_line.setMessage("testMessage");
-		assertTrue(table_line.getMessage() == "testMessage");
+		String str = table_line.getMessage();
+		assertTrue(str == "testMessage");
 
 	}
 
@@ -33,46 +33,47 @@ public class Table_lineTest {
 	public void testSetMessage() throws Exception {
 		String testMessage = "test";
 		table_line.setMessage(testMessage);
-		assertTrue(table_line.getMessage() == "test");
+		assertTrue(table_line.getMessage().equals("test"));
 	}
 
 	@Test
 	public void testGetType() throws Exception {
-		table_line.setType("testType");
-		assertTrue(table_line.getType() == "testType");
+		// table_line.setType("testType");
+		String str = table_line.getType();
+		assertTrue(str == "testType");
 	}
 
 	@Test
 	public void testSetType() throws Exception {
 		String testType = "test";
 		table_line.setType(testType);
-		assertTrue(table_line.getType() == "test");
+		assertTrue(table_line.getType().equals("test"));
 	}
 
 	@Test
 	public void testGetSender() throws Exception {
-		table_line.setSender("testSender");
-		assertTrue(table_line.getSender() == "testSender");
+		String str = table_line.getSender();
+		assertTrue(str == "testSender");
 	}
 
 	@Test
 	public void testSetSender() throws Exception {
 		String testSender = "test";
 		table_line.setSender(testSender);
-		assertTrue(table_line.getSender() == "test");
+		assertTrue(table_line.getSender().equals("test"));
 	}
 
 	@Test
 	public void testGetSource() throws Exception {
-		table_line.setSource("testSource");
-		assertTrue(table_line.getSource() == "testSource");
+		String str = table_line.getSource();
+		assertTrue(str == "testSource");
 	}
 
 	@Test
 	public void testSetSource() throws Exception {
 		String testSource = "test";
 		table_line.setSource(testSource);
-		assertTrue(table_line.getSource() == "test");
+		assertTrue(table_line.getSource().equals("test"));
 	}
 
 }
